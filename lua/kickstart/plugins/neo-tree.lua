50b60e83b68de7511e5109222f384695fddf82a0
Seawhan Lee <seawhanl@gmail.com>
2026-03-09 14:08:14 +0900

    Changed neo-tree to show hidden files or folder except ".git".

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -24,6 +24,14 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        never_show = {
+          '.git',
+        },
+      },
     },
   },
 }
